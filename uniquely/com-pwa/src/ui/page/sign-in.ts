62a164aa31325b59d3127a9dc5e57741880f363c
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-25 14:29:52 +0330

    fix(com-pwa): use service response type for sign in context type

--- uniquely/com-pwa/src/ui/page/sign-in.ts
+++ uniquely/com-pwa/src/ui/page/sign-in.ts
@@ -20,7 +20,7 @@ import '@alwatr/ui-kit/text-field/text-field.js';
 import {sanitizePhoneNumber} from '@alwatr/validator';
 
 import {buttons} from '../../manager/buttons.js';
-import {signIn, userStorageContextConsumer} from '../../manager/context-provider/user.js';
+import {signIn, signInContextConsumer} from '../../manager/context-provider/sign-in.js';
 import {topAppBarContextProvider} from '../../manager/context.js';
 
 import type {AlwatrTextField} from '@alwatr/ui-kit/text-field/text-field.js';
@@ -83,7 +83,7 @@ export class AlwatrPageSignIn extends UnresolvedMixin(SignalMixin(AlwatrBaseElem
   `;
 
   @state()
-  private _userState = userStorageContextConsumer.getState().target;
+  private _userState = signInContextConsumer.getState().target;
 
   private _linkPass: string | null = null;
   private _textInputRef: Ref<AlwatrTextField> = createRef();
@@ -102,8 +102,8 @@ export class AlwatrPageSignIn extends UnresolvedMixin(SignalMixin(AlwatrBaseElem
     });
 
     // prettier-ignore
-    this._addSignalListeners(userStorageContextConsumer.subscribe(() => {
-      this._userState = userStorageContextConsumer.getState().target;
+    this._addSignalListeners(signInContextConsumer.subscribe(() => {
+      this._userState = signInContextConsumer.getState().target;
       if (this._userState === 'complete') {
         if (this._linkPass != null) {
           localStorage.setItem('user-token', this._linkPass);
@@ -126,7 +126,7 @@ export class AlwatrPageSignIn extends UnresolvedMixin(SignalMixin(AlwatrBaseElem
       ];
     }
     else {
-      content = userStorageContextConsumer.fsm.render({
+      content = signInContextConsumer.fsm.render({
         'initial': () => [
           this._renderTextField(),
           this._renderSignInButton(),
@@ -187,7 +187,7 @@ export class AlwatrPageSignIn extends UnresolvedMixin(SignalMixin(AlwatrBaseElem
 
   protected _renderErrorMessage(): unknown {
     this._logger.logMethod?.('_renderErrorMessage');
-    const errorKey = userStorageContextConsumer.getResponse()?.statusCode === 404
+    const errorKey = signInContextConsumer.getResponse()?.statusCode === 404
       ? 'sign_in_error_user_not_found'
       : 'sign_in_error_unknown';
 
